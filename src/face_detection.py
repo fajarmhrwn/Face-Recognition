6c911bee9ca8a898fed91cf40320eb5fd8ff18c2
auliamey <auliaannandya@gmail.com>
2022-11-09 20:28:47 +0700

    convert image

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,2 +1,11 @@
 # pake opencv buat ngambil wajah dr kamera
 # dilanjut sampe konversi greyscale, kebentuk matriks 
+
+import cv2
+
+def convertedImage(imagename):
+    image = cv2.imread(imagename)
+    image = cv2.resize(image, (256, 256), interpolation = cv2.INTER_AREA)
+    greyscaleimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    converted = greyscaleimg.flatten()
+    return converted

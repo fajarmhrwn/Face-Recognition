efab6e122c6f4f616b6349e34700ede246e6e624
auliamey <89340189+auliamey@users.noreply.github.com>
2022-11-12 14:35:32 +0700

    rata2 matrix

--- src/face_detection.py
+++ src/face_detection.py
@@ -2,10 +2,32 @@
 # dilanjut sampe konversi greyscale, kebentuk matriks 
 
 import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-def convertedImage(imagename):
+def convertImage(imagename):
     image = cv2.imread(imagename)
     image = cv2.resize(image, (256, 256), interpolation = cv2.INTER_AREA)
     greyscaleimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     converted = greyscaleimg.flatten()
     return converted
+
+def getMean(pth):
+    mean = [0 for i in range(256*256)]
+    c=0
+    path = r"pins_dataset/" + pth #"pins_dataset" itu nama foldernya
+    temp = os.listdir(path)
+    for file in temp:
+        a = convertImage(pth+"/"+file)
+        c+=1
+        for i in range (256*256):
+            mean[i] += a[i]
+    for i in range (256*256):
+        mean[i] /= c
+    return mean
+
+a = getMean("pins_Jimmy Fallon")
+reshape = np.reshape(a,(256,256))
+plt.imshow(reshape, cmap='gray')
+plt.show()

59d3bbeee6c7410ebbaf2593a43c7126dd39d4e4
Fajar Herawan <13521080@std.stei.itb.ac.id>
2022-11-22 19:51:15 +0700

    requirements update

--- src/ver.py
+++ src/ver.py
@@ -3,6 +3,7 @@ import PIL
 import tkinter
 import numpy as np
 import matplotlib
+import customtkinter
 
 cv_ver = cv2.__version__
 pil_ver = PIL.__version__
@@ -11,3 +12,4 @@ print(pil_ver)
 print(tkinter.TkVersion)
 print(np.__version__)
 print(matplotlib.__version__)
+print(customtkinter.__version__)
